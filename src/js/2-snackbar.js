c5b29702c7b799f6611bfbbd0e4375538e84835c
Oleksandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2026-04-20 19:25:10 +0300

    advanced js homework completed

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(value => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${value}ms`,
+        position: 'topRight',
+      });
+    });
+
+  event.currentTarget.reset();
+});
